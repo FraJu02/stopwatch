2fb588cd46276b58f9af103b255400ebb69b90d0
Julius Franzen <jfranzen@student.tugraz.at>
2022-11-03 14:29:10 +0100

    woop wooop

--- stopwatch.js
+++ stopwatch.js
@@ -1,7 +1,46 @@
-const stopwatch = {hour_s:0, minute_s:0,second_s:0,milli_s:10};
+const stopwatch = {hour_s:0,
+     minute_s:0,
+     second_s:0,
+     milli_s:10,
+     sum: function() {return this.minute_s + " " + this.second_s;}};
+
+     let timer;
 
 function alertTime(){
-    alert(stopwatch.hour_s);
     document.getElementById("minutes").innerHTML = 10;
-    document.getElementById("milliseconds").innerHTML = stopwatch["milli_s"];
+    startButton();
+}
+
+function changeText(){
+    alert("AHHH");
+}
+
+function startButton(){
+    timer = setInterval(countUp, 10);
+}
+
+function stopButton(){
+    clearInterval(timer);
+}
+
+function countUp(){
+    
+    if(stopwatch.milli_s > 900)
+    {
+        stopwatch.milli_s = 0;
+        stopwatch.second_s += 1;
+    }
+    if(stopwatch.second_s >= 59)
+    {
+        stopwatch.second_s = 0;
+    }
+    if(stopwatch.milli_s < 100)
+        document.getElementById("milliseconds").innerHTML = "0" + (stopwatch["milli_s"] / 10);
+    else
+        document.getElementById("milliseconds").innerHTML = (stopwatch["milli_s"] / 10);
+    if(stopwatch.second_s < 10)
+        document.getElementById("seconds").innerHTML = "0" + stopwatch["second_s"];
+    else
+        document.getElementById("seconds").innerHTML = stopwatch["second_s"];
+    stopwatch.milli_s += 10;
 }
